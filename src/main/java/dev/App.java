a05c908485448b1cd792e09d0caa4960ef44d513
Fekhreddine23 <fekhredine1990@gmail.com>
2022-10-24 15:53:38 +0200

    integration librairie tierce

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,5 +1,7 @@
 package dev;
 
+import com.github.lalyos.jfiglet.FigletFont;
+
 /**
  * Hello world!
  *
@@ -8,7 +10,10 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	
+    	String asciiArt = FigletFont.convertOneLine("hello World");
+        System.out.println(asciiArt);
+        
         
     }
 }
